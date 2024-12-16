febe5920495f17e3b86359d264235ede4a3b4edd
Arthur Fernandes Vaz <arthurvaz1805@gmail.com>
2024-12-16 13:59:58 -0300

    Utilização de Componentes em outros modulos

--- src/app/cards/cards.module.ts
+++ src/app/cards/cards.module.ts
@@ -1,13 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardComponent } from '../card/card.component';
+import { CardRoxoButtonComponent } from '../card-roxo-button/card-roxo-button.component';
+import { CardRoxoComponent } from '../card-roxo/card-roxo.component';
+import { CardButtonComponent } from '../card-button/card-button.component';
 
 
 @NgModule({
   declarations: [
-    
+    CardComponent,
+    CardButtonComponent,
+    CardRoxoComponent,
+    CardRoxoButtonComponent
   ],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [
+    CardComponent,
+    CardRoxoButtonComponent,
+    CardRoxoComponent,
+    CardRoxoButtonComponent
+  ],
 })
 export class CardsModule { }
