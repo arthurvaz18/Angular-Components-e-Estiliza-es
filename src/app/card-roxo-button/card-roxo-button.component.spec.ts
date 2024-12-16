2f2d27c6274a1a2420e36d769ffcfc404660d317
Arthur Fernandes Vaz <arthurvaz1805@gmail.com>
2024-12-16 13:31:01 -0300

    Criação de componentes filhos

---
+++ src/app/card-roxo-button/card-roxo-button.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CardRoxoButtonComponent } from './card-roxo-button.component';
+
+describe('CardRoxoButtonComponent', () => {
+  let component: CardRoxoButtonComponent;
+  let fixture: ComponentFixture<CardRoxoButtonComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CardRoxoButtonComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CardRoxoButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
