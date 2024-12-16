f50b4ec18230602a598dc33c9628f06443745485
Arthur Fernandes Vaz <arthurvaz1805@gmail.com>
2024-12-16 15:52:34 -0300

    atualização

---
+++ src/app/card-button-cancel/card-button-cancel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-button-cancel',
+  templateUrl: './card-button-cancel.component.html',
+  styleUrls: ['./card-button-cancel.component.scss']
+})
+export class CardButtonCancelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
