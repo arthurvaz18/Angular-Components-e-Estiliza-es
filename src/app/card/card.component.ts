99d1c120ad59cd8f1da518c5795eb53d0aaa3a27
Arthur Fernandes Vaz <arthurvaz1805@gmail.com>
2024-12-16 14:28:05 -0300

    Processo de Binding

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  tipo = {
+    infos: {
+      plano: 'Simples',
+      preco: 100,
+    }
+  }
+
   constructor() { }
 
   ngOnInit(): void {
