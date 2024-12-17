c8d10b58d940402ac17fe80c62cc11f8ceb1affa
Arthur Fernandes Vaz <155276471+arthurvaz18@users.noreply.github.com>
2024-12-17 12:22:51 -0300

    Atualizações pequenas

--- src/app/card-button-cancel/card-button-cancel.component.ts
+++ src/app/card-button-cancel/card-button-cancel.component.ts
@@ -4,23 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-card-button-cancel',
   templateUrl: './card-button-cancel.component.html',
   styleUrls: ['./card-button-cancel.component.scss'],
-  styles: [
-    `
-    .card-cancel-button {
-      font-size: 16px;
-      text-align: center;
-      margin-top: 10px;
-      border-radius: 25px;
-      background-color: red;
-      cursor: pointer;
-      font-weight: bold;
-      padding: 10px;
-    }
-      .card-cancel-button{
-        color:#fd7557 ;
-    }
-    `
-  ],
+  
 
 })
 export class CardButtonCancelComponent implements OnInit {
