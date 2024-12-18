4156d69248415cc0d3024e72afaedb6d0b1aebf5
Arthur Fernandes Vaz <arthurvaz1805@gmail.com>
2024-12-18 10:50:38 -0300

    Aprendendo sobre ::ng-deep

--- src/app/card-roxo/card-roxo.component.ts
+++ src/app/card-roxo/card-roxo.component.ts
@@ -1,11 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+interface IPlano {
+  infos: IInfos;
+}
+
+interface IInfos {
+  tipo: string;
+  preco: number;
+}
+
 @Component({
   selector: 'app-card-roxo',
   templateUrl: './card-roxo.component.html',
   styleUrls: ['./card-roxo.component.scss']
 })
 export class CardRoxoComponent implements OnInit {
+ 
+  //@ts-ignore
+  plano: IPlano = {
+    infos: {
+      tipo: 'Simples',
+      preco: 200,
+    },
+  };
 
   constructor() { }
 
